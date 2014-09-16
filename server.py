d45d4986a322ade085304466dac969fb42e88197
James Spencer <j.spencer@imperial.ac.uk>
2014-09-16 14:43:53 +0100

    Refactor data output to a separate method.

--- server.py
+++ server.py
@@ -4,8 +4,8 @@ import hashlib
 import os
 import time
 import pandas
-import glob
 import zipfile
+import requests
 
 from json import loads, dumps
 
@@ -15,6 +15,9 @@ CONFIG = {
         'server.socket_host': '0.0.0.0'
     }
 }
+
+with open('mailgun_api') as f:
+    JSS_MAILGUN_TOKEN = f.read().strip()
 
 class RobotPost:
     def index(self, json=None):
@@ -38,19 +41,7 @@ class RobotPost:
 
             # Output!
             path = 'files' + os.sep + json_obj['UserHash'] + os.sep + timestamp
-            d = os.path.dirname(path)
-            if not os.path.exists(d):
-                os.makedirs(d)
-            f = open('%.json' % path, 'w')
-            f.write(dumps(json_obj, indent=2))
-            f.close()
-
-            data = pandas.read_json(json_obj, typ='series')
-            data.to_frame().to_excel('%.xsl' % path, 'w')
-            data.to_csv('%s.csv' % path)
-            f = open('%s.txt' % path, 'w')
-            f.write(data.to_string())
-            f.close()
+            self.dump_data(json_obj['UserHash'], path)
 
         m = hashlib.md5()
         m.update(result['Status'] + result['Payload'])
@@ -60,6 +51,26 @@ class RobotPost:
         cherrypy.response.headers['Content-Type']= 'application/json'
 
         return dumps(result, indent=2)
+
+    def dump_data(self, user_hash, stem):
+
+            d = os.path.dirname(stem)
+            if not os.path.exists(d):
+                os.makedirs(d)
+            f = open('%.json' % stem, 'w')
+            f.write(dumps(json_obj, indent=2))
+            f.close()
+
+            data = pandas.read_json(json_obj, typ='series')
+            data.to_frame().to_excel('%.xsl' % stem, 'w')
+            data.to_csv('%s.csv' % stem)
+            f = open('%s.txt' % stem, 'w')
+            f.write(data.to_string())
+            f.close()
+
+    def email_data(self, user_hash, stem):
+
+        pass
 
     index.exposed = True
 
